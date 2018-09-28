17413dc68e45cc911e69781a97a92294d6d2a78e
chalk777 <igor.meleshko@gmail.com>
2018-09-28 19:44:18 +0300

    Initial commit

---
+++ classes/XLite/Module/Coinbase/CoinbaseCommercePayment/Controller/Customer/CoinbaseIpn.php
@@ -0,0 +1,113 @@
+<?php
+namespace XLite\Module\Coinbase\CoinbaseCommercePayment\Controller\Customer;
+
+class CoinbaseIpn extends \XLite\Controller\Customer\ACustomer
+{
+    const EVENT_FAILED = 'charge:failed';
+    const EVENT_DELAYED = 'charge:delayed';
+    const EVENT_CONFIRMED = 'charge:confirmed';
+
+    public function __construct($params = array())
+    {
+        parent::__construct($params);
+        \XLite\Module\Coinbase\CoinbaseCommercePayment\Main::includeLibrary();
+    }
+
+    protected function doNoAction()
+    {
+        $body = file_get_contents('php://input');
+        $bodyArray = \json_decode($body, true);
+
+        if (!isset($bodyArray['event'])) {
+            $this->markCallbackRequestAsInvalid('Invalid payload provided. No event key.');
+
+            return false;
+        }
+
+        $event = new \CoinbaseSDK\Resources\Event($bodyArray['event']);
+        $charge = $event->data;
+
+        $publicTxnId = $charge->getMetadataParam(METADATA_INVOICEID_PARAM);
+
+        $transaction = $this->loadTransaction($publicTxnId);
+
+        $secretKey = $transaction->getPaymentMethod()->getSetting('secret_key');
+        $headers = array_change_key_case(getallheaders());
+        $signatureHeader = isset($headers[SIGNATURE_HEADER]) ? $headers[SIGNATURE_HEADER] : null;
+
+        try {
+            \CoinbaseSDK\Webhook::verifySignature($body, $signatureHeader, $secretKey);
+        } catch (\Exception $exception) {
+            $this->markCallbackRequestAsInvalid($exception->getMessage());
+        }
+
+        if ($transaction->getDataCell(METADATA_TOKEN_PARAM) != $charge->getMetadataParam(METADATA_TOKEN_PARAM)) {
+            $this->markCallbackRequestAsInvalid('Invalid token.');
+        }
+
+        switch ($event->type) {
+            case self::EVENT_FAILED:
+            case self::EVENT_DELAYED:
+                $transaction->setStatus($transaction::STATUS_FAILED);
+                \XLite\Core\Database::getEM()->flush();
+
+                break;
+            case self::EVENT_CONFIRMED:
+                $transactionId = '';
+                $total = '';
+                $currency = '';
+
+                foreach ($charge->payments as $payment) {
+                    if (strtolower($payment['status']) === 'confirmed') {
+                        $transactionId = $payment['transaction_id'];
+                        $total = isset($payment['value']['local']['amount']) ? $payment['value']['local']['amount'] : $total;
+                        $currency = isset($payment['value']['local']['currency']) ? $payment['value']['local']['currency'] : $currency;
+                    }
+                }
+                $transaction->setDataCell('remote_txn', $transactionId, 'Founded coinbase commerce transaction');
+                $transaction->setStatus($transaction::STATUS_SUCCESS);
+                \XLite\Core\Database::getEM()->flush();
+
+                $cart = $transaction->getOrder();
+
+                if ($cart instanceof \XLite\Model\Cart) {
+                    $cart->tryClose();
+                }
+
+                $transaction->getOrder()->setPaymentStatusByTransaction($transaction);
+                $transaction->getOrder()->update();
+                break;
+            default:
+                $transaction->setStatus($transaction::STATUS_PENDING);
+                \XLite\Core\Database::getEM()->flush();
+        }
+
+    }
+
+    private function loadTransaction($publicTxnId)
+    {
+        $transaction = \Xlite\Core\Database::getRepo('XLite\Model\Payment\Transaction')->findOneBy(
+            ['public_id' => $publicTxnId]
+        );
+
+        if (!$transaction) {
+            $this->markCallbackRequestAsInvalid('Transaction was not found. Transaction Id:' . $publicTxnId);
+        }
+
+        return $transaction;
+    }
+
+    /**
+     * Mark callback request as invalid
+     *
+     * @param string $message Message
+     */
+    public function markCallbackRequestAsInvalid($message)
+    {
+        \XLite\Logger::getInstance()->log(
+            'Callback request is invalid: ' . $message . PHP_EOL
+        );
+
+        die();
+    }
+}
---
+++ classes/XLite/Module/Coinbase/CoinbaseCommercePayment/Model/Payment/Processor/CoinbaseCommercePayment.php
@@ -0,0 +1,228 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+/**
+ * Copyright (c) 2011-present Qualiteam software Ltd. All rights reserved.
+ * See https://www.x-cart.com/license-agreement.html for license details.
+ */
+
+namespace XLite\Module\Coinbase\CoinbaseCommercePayment\Model\Payment\Processor;
+
+/**
+ * Payment processor for coinbase
+ */
+class CoinbaseCommercePayment extends \XLite\Model\Payment\Base\WebBased
+{
+    /**
+     * Constructor
+     */
+    protected function __construct()
+    {
+        parent::__construct();
+
+        \XLite\Module\Coinbase\CoinbaseCommercePayment\Main::includeLibrary();
+    }
+
+    /**
+     * Process return
+     *
+     * @param \XLite\Model\Payment\Transaction $transaction Return-owner transaction
+     *
+     * @return void
+     */
+    public function processReturn(\XLite\Model\Payment\Transaction $transaction)
+    {
+        parent::processReturn($transaction);
+
+        if (\XLite\Core\Request::getInstance()->cancel) {
+            $this->setDetail(
+                'status',
+                'Customer has canceled checkout before completing their payments',
+                'Status'
+            );
+            $this->transaction->setNote('Customer has canceled checkout before completing their payments');
+            $this->transaction->setStatus($transaction::STATUS_CANCELED);
+
+        } elseif ($transaction::STATUS_INPROGRESS == $this->transaction->getStatus()) {
+            $this->transaction->setStatus($transaction::STATUS_PENDING);
+        }
+
+        \XLite\Core\Operator::redirect(
+            $this->getInvoiceUrl($transaction)
+        );
+    }
+
+    private function getInvoiceUrl($transaction)
+    {
+
+        return \XLite::getInstance()->getShopURL(
+            \XLite\Core\Converter::buildURL('order', '', array('order_number' => $transaction->getOrder()->getOrderNumber())),
+            \XLite\Core\Config::getInstance()->Security->customer_security
+        );
+    }
+
+    /**
+     * Get settings widget or template
+     *
+     * @return string Widget class name or template path
+     */
+    public function getSettingsWidget()
+    {
+        return 'modules/Coinbase/CoinbaseCommercePayment/config.twig';
+    }
+
+    /**
+     * Check - payment method is configured or not
+     *
+     * @param \XLite\Model\Payment\Method $method Payment method
+     *
+     * @return boolean
+     */
+    public function isConfigured(\XLite\Model\Payment\Method $method)
+    {
+        return parent::isConfigured($method)
+          && $method->getSetting('app_key')
+          && $method->getSetting('secret_key');
+    }
+
+    /**
+     * Get payment method admin zone icon URL
+     *
+     * @param \XLite\Model\Payment\Method $method Payment method
+     *
+     * @return string
+     */
+    public function getAdminIconURL(\XLite\Model\Payment\Method $method)
+    {
+        return true;
+    }
+
+    /**
+     * Returns the list of settings available for this payment processor
+     *
+     * @return array
+     */
+    public function getAvailableSettings()
+    {
+        return array(
+            'app_key',
+            'secret_key',
+        );
+    }
+
+    /**
+     * Get redirect form URL
+     *
+     * @return string
+     */
+    protected function getFormURL()
+    {
+        $token = $this->generateRandomToken();
+        $billingAddress = $this->getProfile()->getBillingAddress();
+        $chargeData = array(
+            'local_price' => array(
+                'amount' => $this->formatCurrency($this->transaction->getValue()),
+                'currency' => $this->getCurrencyCode()
+            ),
+            'pricing_type' => 'fixed_price',
+            'name' => sprintf(
+                '%s: payment for %s transaction',
+                \XLite\Core\Config::getInstance()->Company->company_name,
+                $this->transaction->getPublicTxnId()
+            ),
+            'description' => implode($this->getOrderProducts(), ', '),
+            'metadata' => [
+                METADATA_SOURCE_PARAM => METADATA_SOURCE_VALUE,
+                METADATA_INVOICEID_PARAM  => $this->transaction->getPublicTxnId(),
+                METADATA_TOKEN_PARAM => $token,
+                'email' => $this->getProfile()->getLogin(),
+                'firstName' => $billingAddress ? $billingAddress->getFirstname() : '',
+                'lastName' => $billingAddress ? $billingAddress->getLastname() : ''
+            ],
+            'redirect_url' => $this->getReturnURL(null, true)
+        );
+
+        $this->transaction->setDataCell(METADATA_TOKEN_PARAM, $token, METADATA_TOKEN_PARAM);
+
+        \CoinbaseSDK\ApiClient::init($this->getSetting('app_key'));
+        $chargeObj = \CoinbaseSDK\Resources\Charge::create($chargeData);
+
+        $this->transaction->order->processSucceed();
+
+        return $chargeObj->hosted_url;
+    }
+
+    private function generateRandomToken()
+    {
+        return bin2hex(random_bytes(4));
+    }
+
+    private function getOrderProducts()
+    {
+        $products = array();
+
+        foreach ($this->transaction->getOrder()->getItems() as $item) {
+            $products[] = $item->getName() . ' x ' . $item->getAmount();
+        }
+
+        return $products;
+    }
+
+    /**
+     * Get redirect form fields list
+     *
+     * @return array
+     */
+    protected function getFormFields()
+    {
+        return array(
+            'transactionID' => $this->transaction->getPublicTxnId(),
+            'returnURL' => $this->getReturnURL('transactionID')
+        );
+    }
+
+    /**
+     * Get Webhook URL
+     *
+     * @return string
+     */
+    public function getWebhookURL()
+    {
+        return \XLite::getInstance()->getShopURL(
+            \XLite\Core\Converter::buildURL('coinbase_ipn', null, array(), \XLite::getCustomerScript()),
+            \XLite\Core\Config::getInstance()->Security->customer_security
+        );
+    }
+
+    /**
+     * Format currency
+     *
+     * @param float $value Currency value
+     *
+     * @return integer
+     */
+    protected function formatCurrency($value)
+    {
+        return $this->transaction->getCurrency()->roundValueAsInteger($value);
+    }
+
+    /**
+     * Get form method
+     *
+     * @return string
+     */
+    protected function getFormMethod()
+    {
+        return static::FORM_METHOD_GET;
+    }
+
+    /**
+     * Get currency code
+     *
+     * @return string
+     */
+    protected function getCurrencyCode()
+    {
+        return strtoupper($this->transaction->getCurrency()->getCode());
+    }
+}
