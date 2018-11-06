964b00ae1c0d35cc554a6e1a9a977a87336d3a48
Igor Meleshko <imeleshko123@gmail.com>
2018-11-06 23:59:06 +0200

    Added  xcart plugin version 1.0 (#1)
    * Initial commit
    * Added plugin version 1.0
    * Fix price calc
    * Added cancellation, refactoredwebhook processing
    *  Remove dalay logic

---
+++ classes/XLite/Module/Coinbase/CoinbaseCommercePayment/Main.php
@@ -0,0 +1,100 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+/**
+ * Copyright (c) 2011-present Qualiteam software Ltd. All rights reserved.
+ * See https://www.x-cart.com/license-agreement.html for license details.
+ */
+
+namespace XLite\Module\Coinbase\CoinbaseCommercePayment;
+
+/**
+ * Main module
+ */
+abstract class Main extends \XLite\Module\AModule
+{
+    /**
+     * Author name
+     *
+     * @return string
+     */
+    public static function getAuthorName()
+    {
+        return 'Coinbase';
+    }
+
+    /**
+     * Module name
+     *
+     * @return string
+     */
+    public static function getModuleName()
+    {
+        return 'Coinbase Commerce Payment Method';
+    }
+
+    /**
+     * Module description
+     *
+     * @return string
+     */
+    public static function getDescription()
+    {
+        return 'Coinbase Commerce - Bitcoin/Bitcoin Cash/Litecoin/Ethereum Payments';
+    }
+
+    /**
+     * Get module major version
+     *
+     * @return string
+     */
+    public static function getMajorVersion()
+    {
+        return '5.3';
+    }
+
+    /**
+     * Module version
+     *
+     * @return string
+     */
+    public static function getMinorVersion()
+    {
+        return '0';
+    }
+
+
+    public static function getBuildVersion()
+    {
+        return '5';
+    }
+
+    /**
+     * Get minor core version which is required for the module activation
+     *
+     * @return string
+     */
+    public static function getMinorRequiredCoreVersion()
+    {
+        return '3';
+    }
+
+    /**
+     * Get module type
+     *
+     * @return integer
+     */
+    public static function getModuleType()
+    {
+        return static::MODULE_TYPE_PAYMENT;
+    }
+
+    /**
+     * Include libraries
+     */
+    public static function includeLibrary()
+    {
+        require_once LC_DIR_MODULES . str_replace('/', LC_DS, 'Coinbase/CoinbaseCommercePayment/Logic/CoinbaseSDK/init.php');
+        require_once LC_DIR_MODULES . str_replace('/', LC_DS, 'Coinbase/CoinbaseCommercePayment/Logic/CoinbaseSDK/const.php');
+    }
+}
